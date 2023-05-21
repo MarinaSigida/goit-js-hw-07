840085983acb3ef423311329b09f2d0973bda26f
MarinaSigida <sigida.web@gmail.com>
2023-05-21 20:09:40 +0200

    update1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+    const galleryItem =  galleryItems.map(({preview, original, description})=> {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </li>
+  `;
+    });
+
+    console.log(galleryItem);
+
+    gallery.insertAdjacentHTML('beforeend', galleryItem.join(''));
+
+    gallery.addEventListener('click', onImageClick);
+
+    function onImageClick (event) {
+        event.preventDefault();
+        if (event.target.nodeName !== 'IMG'){
+            return;
+        };
+
+// Open modal window with basicligthbox
+        const instance = basicLightbox.create (`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+        `)
+        instance.show();
+
+// Close modal window with basicligthbox
+
+        gallery.addEventListener('keydown', (event) =>{
+            if (event.code === 'Escape') {
+                instance.close();
+            }
+        });
+    };
+
+        
+   
+
+
